a82d77c0d78fbcaad09fd244c26d5d4b6a7c24ef
Lauricio Melhor Programador do Universo <lauriciomalek@gmail.com>
2024-09-20 08:37:24 -0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,91 @@
+-- Arquivo: themes.lua
+-- Requer 'vim-plug' ou outro gerenciador de plugins configurado
+
+-- Primeiro, certifique-se de ter o gerenciador de plugins configurado em seu 'init.lua' ou 'init.vim'.
+
+-- Use o comando :PlugInstall após descomentar algum tema para instalá-lo, se necessário.
+
+-- Exemplos de temas:
+
+-- 1. Gruvbox
+-- Plug 'morhetz/gruvbox'
+-- vim.cmd('colorscheme gruvbox')
+
+-- 2. Nord
+-- Plug 'shaunsingh/nord.nvim'
+-- vim.cmd('colorscheme nord')
+
+-- 3. Dracula
+-- Plug 'dracula/vim', { 'as': 'dracula' }
+-- vim.cmd('colorscheme dracula')
+
+-- 4. Solarized
+-- Plug 'altercation/vim-colors-solarized'
+-- vim.cmd('colorscheme solarized')
+
+-- 5. OneDark
+-- Plug 'joshdick/onedark.vim'
+-- vim.cmd('colorscheme onedark')
+
+-- 6. PaperColor
+-- Plug 'NLKNguyen/papercolor-theme'
+-- vim.cmd('colorscheme PaperColor')
+
+-- 7. Nightfox
+-- Plug 'EdenEast/nightfox.nvim'
+-- vim.cmd('colorscheme nightfox')
+
+-- 8. Catppuccin
+-- Plug 'catppuccin/nvim', { 'as': 'catppuccin' }
+-- vim.cmd('colorscheme catppuccin')
+
+-- 9. TokyoNight
+-- Plug 'folke/tokyonight.nvim'
+-- vim.cmd('colorscheme tokyonight')
+
+-- 10. Ayu
+-- Plug 'ayu-theme/ayu-vim'
+-- vim.cmd('colorscheme ayu')
+
+-- 11. Zenbones
+-- Plug 'mcchrish/zenbones.nvim'
+-- vim.cmd('colorscheme zenbones')
+
+-- 12. Iceberg
+-- Plug 'cocopon/iceberg.vim'
+-- vim.cmd('colorscheme iceberg')
+
+-- 13. Material
+-- Plug 'marko-cerovac/material.nvim'
+-- vim.cmd('colorscheme material')
+
+-- 14. Horizon
+-- Plug 'NTBBloodbath/doom-one.nvim'
+-- vim.cmd('colorscheme horizon')
+
+-- 15. Palenight
+-- Plug 'drewtempelmeyer/palenight.vim'
+-- vim.cmd('colorscheme palenight')
+
+-- 16. Darkplus
+-- Plug 'LunarVim/darkplus.nvim'
+-- vim.cmd('colorscheme darkplus')
+
+-- 17. SpaceCamp
+-- Plug 'jacoborus/tender.vim'
+-- vim.cmd('colorscheme tender')
+
+-- 18. Edge
+-- Plug 'sainnhe/edge'
+-- vim.cmd('colorscheme edge')
+
+-- 19. Everforest
+-- Plug 'sainnhe/everforest'
+-- vim.cmd('colorscheme everforest')
+
+-- 20. OceanicNext
+-- Plug 'mhartington/oceanic-next'
+-- vim.cmd('colorscheme OceanicNext')
+
+-- Para ativar um tema, descomente a linha do 'Plug' e do 'vim.cmd'.
+-- Lembre-se de rodar :PlugInstall para baixar o tema.
